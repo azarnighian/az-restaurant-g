fadcdf08c4471508a0bd9360d21d3e675d485095
Aharon <aharonzarnighian@gmail.com>
2021-06-17 15:08:10 -0400

    Partially added landing page component

---
+++ src/components/Landing.js
@@ -0,0 +1,13 @@
+const Landing = () => {
+  return (
+    <div className="LandingContainer">        
+        <div className="LandingText">
+            <h1>Restaurant</h1>
+            <h2>Description about restaurant</h2>
+        </div>
+        <div className="LandingImage"></div>
+    </div>
+  )
+}
+
+export default Landing
