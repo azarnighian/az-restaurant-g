524324331567242f5bd6ed5778f65df70132d9f3
Aharon <aharonzarnighian@gmail.com>
2021-06-22 13:27:47 -0400

    HomeLanding styling and font

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,8 +1,7 @@
 const HomeAbout = () => {
   return (
     <div className="container">        
-        <h2>Welcome to</h2>
-        <h1>Restaurant</h1>
+        <h1>About Us</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu augue ut lectus arcu bibendum. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Ut venenatis tellus in metus vulputate eu scelerisque felis. Quis auctor elit sed vulputate mi sit amet mauris commodo. Feugiat pretium nibh ipsum consequat nisl vel. Leo vel fringilla est ullamcorper eget. Imperdiet massa tincidunt nunc pulvinar sapien et ligula. Interdum velit laoreet id donec ultrices tincidunt arcu non. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.</p>
         <button>Learn More</button>
     </div>
--- src/components/HomeLanding.js
+++ src/components/HomeLanding.js
@@ -2,7 +2,7 @@ const HomeLanding = () => {
   return (
     <div className="landingContainer">                
       <h1>Welcome to Restaurant</h1>
-      <h2>Description about restaurant</h2>
+      <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
     </div>
   )
 }
