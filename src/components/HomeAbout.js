ef0828a7bd9cf165c4f5e31acf056744786dd6e1
Aharon <aharonzarnighian@gmail.com>
2021-06-24 13:34:00 -0400

    Added color, styled buttons, made menu image more responsive

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -3,7 +3,7 @@ const HomeAbout = () => {
     <div className="container">        
         <h1>About Us</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Eu augue ut lectus arcu bibendum. Sed turpis tincidunt id aliquet risus feugiat in ante metus. Ut venenatis tellus in metus vulputate eu scelerisque felis. Quis auctor elit sed vulputate mi sit amet mauris commodo. Feugiat pretium nibh ipsum consequat nisl vel. Leo vel fringilla est ullamcorper eget. Imperdiet massa tincidunt nunc pulvinar sapien et ligula. Interdum velit laoreet id donec ultrices tincidunt arcu non. Suspendisse interdum consectetur libero id faucibus nisl tincidunt.</p>
-        <button>Learn More</button>
+        <button>LEARN MORE</button>
     </div>
   )
 }
--- src/components/HomeMenu.js
+++ src/components/HomeMenu.js
@@ -5,7 +5,7 @@ const HomeMenu = () => {
         <div className="menuTextContainer">
             <h1>Our Menu</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-            <button>Menu</button>
+            <button>MENU</button>
         </div>        
     </div>
   )
