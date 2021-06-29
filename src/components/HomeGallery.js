8d5d7cb54b07eab1747827284ef07625c240cbe1
Aharon <aharonzarnighian@gmail.com>
2021-06-29 11:40:52 -0400

    Added images folder, mostly finished HomeGallery.js

--- src/components/HomeGallery.js
+++ src/components/HomeGallery.js
@@ -1,10 +1,13 @@
+import image1 from './images/pexels-anna-tis-6341164.jpg';
+import image2 from './images/pexels-asad-photo-maldives-1449773.jpg';
+
 const HomeGallery = () => {
   return (
     <div className="container beigeBackground">        
         <h1>Gallery</h1>
         <div className="homeGalleryContainer">
-            <img src="https://images.pexels.com/photos/1449773/pexels-photo-1449773.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Homepage Gallery 1"></img>
-            <img src="https://images.pexels.com/photos/6341164/pexels-photo-6341164.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Homepage Gallery 2"></img>            
+            <img src={image1} alt="Homepage Gallery 1"></img>
+            <img src={image2} alt="Homepage Gallery 2"></img>            
         </div>        
         <button>GALLERY</button>
     </div>
