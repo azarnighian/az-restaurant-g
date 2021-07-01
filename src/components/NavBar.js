ea125f585fef75d43ba4e05944ec3a7797ce8e2b
Aharon <aharonzarnighian@gmail.com>
2021-07-01 17:58:05 -0400

    Added active class to nav links using useLocation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,10 @@
 import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
+    // See 1:46:15 of here: https://www.youtube.com/watch?v=w7ejDZ8SWv8&t=621s
 
 const NavBar = () => {
+  const location = useLocation()
+    
   return (
     // Learned from here: https://www.youtube.com/watch?v=gXkqy0b4M5g&t=837s
     <nav>
@@ -17,16 +21,16 @@ const NavBar = () => {
                 <Link to="/">Home</Link>
             </li>                    
             <li>
-                <Link to="/about">About</Link>
+                <Link to="/about" className={location.pathname === "/about" ? "active" : ""}>About</Link>
             </li>
             <li>
-                <Link to="/menu">Menu</Link>
+                <Link to="/menu" className={location.pathname === "/menu" ? "active" : ""}>Menu</Link>
             </li>
             <li>
-                <Link to="/gallery">Gallery</Link>
+                <Link to="/gallery" className={location.pathname === "/gallery" ? "active" : ""}>Gallery</Link>
             </li>
             <li>
-                <Link to="/contact">Contact</Link>
+                <Link to="/contact" className={location.pathname === "/contact" ? "active" : ""}>Contact</Link>
             </li>
         </ul>            
     </nav>
