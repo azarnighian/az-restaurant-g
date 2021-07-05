0223446aa4e352fa2bbc99c504cc31d8c7106543
Aharon <aharonzarnighian@gmail.com>
2021-07-05 14:04:22 -0400

    Added photos to gallery

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,5 +1,15 @@
-import image1 from './galleryImages/pexels-anna-tis-6341164.jpg';
-import image2 from './galleryImages/pexels-asad-photo-maldives-1449773.jpg';
+import image1 from './galleryImages/pexels-asad-photo-maldives-1449773.jpg';
+import image2 from './galleryImages/pexels-daria-shevtsova-704982.jpg';
+import image3 from './galleryImages/pexels-de-lemster-krant-2894275.jpg';
+import image4 from './galleryImages/pexels-life-of-pix-67468.jpg';
+import image5 from './galleryImages/pexels-maria-orlova-4947288.jpg';
+import image6 from './galleryImages/pexels-pixabay-265920.jpg';
+import image7 from './galleryImages/pexels-pixabay-276720.jpg';
+import image8 from './galleryImages/pexels-ruslan-khmelevsky-5392152.jpg';
+import image9 from './galleryImages/pexels-skitterphoto-3498.jpg';
+import image10 from './galleryImages/pexels-skitterphoto-9315.jpg';
+import image11 from './galleryImages/pexels-tom-balabaud-1579715.jpg';
+import image12 from './galleryImages/pexels-waldemar-brandt-2290070.jpg';
 
 // https://github.com/michelecocuccio/simple-react-lightbox
 import { SRLWrapper } from "simple-react-lightbox";
@@ -34,16 +44,16 @@ const Gallery = () => {
           <SRLWrapper options={options}>
             <img src={image1} alt="Gallery 1"></img>
             <img src={image2} alt="Gallery 2"></img>
-            <img src={image1} alt="Gallery 1"></img>
-            <img src={image2} alt="Gallery 2"></img>
-            <img src={image1} alt="Gallery 1"></img>
-            <img src={image2} alt="Gallery 2"></img>
-            <img src={image1} alt="Gallery 1"></img>
-            <img src={image2} alt="Gallery 2"></img>
-            <img src={image1} alt="Gallery 1"></img>
-            <img src={image2} alt="Gallery 2"></img>
-            <img src={image1} alt="Gallery 1"></img>
-            <img src={image2} alt="Gallery 2"></img>
+            <img src={image3} alt="Gallery 3"></img>
+            <img src={image4} alt="Gallery 4"></img>
+            <img src={image5} alt="Gallery 5"></img>
+            <img src={image6} alt="Gallery 6"></img>
+            <img src={image7} alt="Gallery 7"></img>
+            <img src={image8} alt="Gallery 8"></img>
+            <img src={image9} alt="Gallery 9"></img>
+            <img src={image10} alt="Gallery 10"></img>
+            <img src={image11} alt="Gallery 11"></img>
+            <img src={image12} alt="Gallery 12"></img>
           </SRLWrapper>          
         </div>      
       </div>
