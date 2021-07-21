d7e289a6d582034cb9510bbbeed6de1129189c5d
Aharon <aharonzarnighian@gmail.com>
2021-07-21 15:05:02 -0400

    Went back to transitions for nav bar

--- src/landingNavBarStuff.js
+++ src/landingNavBarStuff.js
@@ -2,7 +2,7 @@ export const changeNavBar = () => {
     const scrolled = document.body.scrollTop > 50 || document.documentElement.scrollTop > 50;        
 
     if (scrolled) {
-        document.querySelector('.landingNavBar').className += " navScrolled";
+        document.querySelector('.landingNavBar').className += " landingNavScrolled";
     }
     else {
         document.querySelector('.landingNavBar').className = " landingNavBar";
@@ -11,55 +11,13 @@ export const changeNavBar = () => {
 
 export const landingNavSlide = () => {
     const landingNav = document.querySelector('.landingNavBar');
-    const landingNavLogo = document.querySelector('.landingLogo');
     const landingBurger = document.querySelector('.landingBurger');
-    const landingBurgerLines = document.querySelectorAll('.landingBurger div');
-    const landingNavLinksBackground = document.querySelector('.landingNavLinks');
-    const landingNavLinks = document.querySelectorAll('.landingNavLinks li');
     
     landingBurger.addEventListener('click', () => {
         document.body.classList.toggle('stopScrolling');
+                
+        landingNav.classList.toggle('landingBurgerClicked');                
         
-        // Animate nav header
-        // landingNav.classList.toggle('burgerClicked');
-        if (landingNav.style.animation) {
-            landingNav.style.animation = '';
-        }
-        else {
-            landingNav.style.animation = 'landingNavHeaderFade 0.7s ease forwards';
-        }
-        // Animate logo
-        if (landingNavLogo.style.animation) {
-            landingNavLogo.style.animation = '';
-        }
-        else {
-            landingNavLogo.style.animation = 'landingNavLogoFade 0.7s ease forwards';
-        }
-        // Animate burger
-        landingBurgerLines.forEach((line, index) => {
-            if (line.style.animation) {
-                line.style.animation = '';
-            } else {
-                line.style.animation = 'landingNavBurgerFade 0.7s ease forwards';
-            }
-        });
-        
-        // Animate links background
-        if (landingNavLinksBackground.style.animation) {
-            landingNavLinksBackground.style.animation = '';
-        }
-        else {
-            landingNavLinksBackground.style.animation = 'landingNavFade 0.7s ease forwards';
-        }
-        
-        // Animate Links
-        landingNavLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `landingNavLinkFade 0.7s ease forwards ${index / 7}s`;
-            }
-        });
         // Burger Animation
         landingBurger.classList.toggle('landingToggle');
     });
