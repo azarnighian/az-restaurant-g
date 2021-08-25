f75279ca760dd46fe7b37c7e7cdd5f6b93707f6b
Aharon <aharonzarnighian@gmail.com>
2021-08-25 14:26:59 -0400

    Added down arrow and colored fade to landing page

--- src/components/Home/HomeLanding.js
+++ src/components/Home/HomeLanding.js
@@ -11,6 +11,7 @@ const HomeLanding = () => {
     <div className="landingContainer">                
       <h1>Welcome to Restaurant</h1>
       <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+      <div className="downArrow"></div>
     </div>
   )
 }
